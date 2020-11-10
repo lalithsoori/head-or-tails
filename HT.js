48bebb521b772b048fab143168080c195782c57b
lalithsoori <68772983+lalithsoori@users.noreply.github.com>
2020-11-10 13:55:36 +0530

    Add files via upload

---
+++ HT.js
@@ -0,0 +1,39 @@
+function headTail() {
+  var computerchoice;
+
+  computerchoice = resultToChoose( randomNumber() );
+  console.log( computerchoice );
+
+  result( computerchoice);
+
+  //document.getElementById('gameresultfunction').innerHTML= computerchoice;
+
+}
+
+
+
+function randomNumber() {
+  return Math.floor( Math.random() * 2 );
+}
+
+function resultToChoose( number ) {
+  return [ 'head', 'tail' ][ number ];
+}
+
+
+
+function result( computerchoice, message ) {
+  var imageData = {
+    'head': document.getElementById( 'letterH' ).src,
+    'tail': document.getElementById( 'letterL' ).src
+  }
+
+  var resultDiv = document.createElement( 'div' );
+
+
+  resultDiv.innerHTML = " <img src='" + imageData[ computerchoice ] + "'>"
+
+  document.getElementById( 'gameresultfunction' ).appendChild( resultDiv );
+
+
+}
